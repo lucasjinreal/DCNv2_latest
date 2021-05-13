a4b9f09a8aa0f5f9991d71bb3d26c45fa45e3558
FagangJin <fagangjin@autox.ai>
2021-05-13 11:23:09 +0800

    rm THCudaBlas_SgemmBatched for support pytorch1.8.1

--- src/cuda/dcn_v2_psroi_pooling_cuda.cu
+++ src/cuda/dcn_v2_psroi_pooling_cuda.cu
@@ -281,9 +281,9 @@ dcn_v2_psroi_pooling_cuda_forward(const at::Tensor &input,
                                   const int sample_per_part,
                                   const float trans_std)
 {
-  AT_ASSERTM(input.type().is_cuda(), "input must be a CUDA tensor");
-  AT_ASSERTM(bbox.type().is_cuda(), "rois must be a CUDA tensor");
-  AT_ASSERTM(trans.type().is_cuda(), "trans must be a CUDA tensor");
+  AT_ASSERTM(input.is_cuda(), "input must be a CUDA tensor");
+  AT_ASSERTM(bbox.is_cuda(), "rois must be a CUDA tensor");
+  AT_ASSERTM(trans.is_cuda(), "trans must be a CUDA tensor");
 
   const int batch = input.size(0);
   const int channels = input.size(1);
@@ -314,7 +314,7 @@ dcn_v2_psroi_pooling_cuda_forward(const at::Tensor &input,
   dim3 grid(std::min(THCCeilDiv(out_size, 512L), 4096L));
   dim3 block(512);
 
-  AT_DISPATCH_FLOATING_TYPES(input.type(), "dcn_v2_psroi_pooling_cuda_forward", [&] {
+  AT_DISPATCH_FLOATING_TYPES(input.scalar_type(), "dcn_v2_psroi_pooling_cuda_forward", [&] {
     DeformablePSROIPoolForwardKernelCuda<scalar_t><<<grid, block, 0, stream>>>(
         out_size,
         input.contiguous().data_ptr<scalar_t>(),
@@ -355,11 +355,11 @@ dcn_v2_psroi_pooling_cuda_backward(const at::Tensor &out_grad,
                                    const int sample_per_part,
                                    const float trans_std)
 {
-  AT_ASSERTM(out_grad.type().is_cuda(), "out_grad must be a CUDA tensor");
-  AT_ASSERTM(input.type().is_cuda(), "input must be a CUDA tensor");
-  AT_ASSERTM(bbox.type().is_cuda(), "bbox must be a CUDA tensor");
-  AT_ASSERTM(trans.type().is_cuda(), "trans must be a CUDA tensor");
-  AT_ASSERTM(top_count.type().is_cuda(), "top_count must be a CUDA tensor");
+  AT_ASSERTM(out_grad.is_cuda(), "out_grad must be a CUDA tensor");
+  AT_ASSERTM(input.is_cuda(), "input must be a CUDA tensor");
+  AT_ASSERTM(bbox.is_cuda(), "bbox must be a CUDA tensor");
+  AT_ASSERTM(trans.is_cuda(), "trans must be a CUDA tensor");
+  AT_ASSERTM(top_count.is_cuda(), "top_count must be a CUDA tensor");
 
   const int batch = input.size(0);
   const int channels = input.size(1);
@@ -388,7 +388,7 @@ dcn_v2_psroi_pooling_cuda_backward(const at::Tensor &out_grad,
   dim3 block(512);
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
-  AT_DISPATCH_FLOATING_TYPES(out_grad.type(), "dcn_v2_psroi_pooling_cuda_backward", [&] {
+  AT_DISPATCH_FLOATING_TYPES(out_grad.scalar_type(), "dcn_v2_psroi_pooling_cuda_backward", [&] {
     DeformablePSROIPoolBackwardAccKernelCuda<scalar_t><<<grid, block, 0, stream>>>(
         out_size,
         out_grad.contiguous().data_ptr<scalar_t>(),
